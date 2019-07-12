9116d888c5bb9014b636f3257051e8fe4e6b64f2
Irakli Murusidze <iraklimurusidze@live.com>
2019-07-13 02:52:53 +0400

    Added Settings Content

--- src/Brisk.Web/wwwroot/js/site.js
+++ src/Brisk.Web/wwwroot/js/site.js
@@ -1,1 +1,16 @@
-﻿
+﻿var content = $('#content-container');
+var settings = $('#settings-container');
+
+$('#settings-btn').on('click', function () {
+    content.addClass('hidden');
+    settings.removeClass('hidden');
+    $('#settings-btn').addClass('active');
+    $('#new-game-btn').removeClass('active');
+});
+
+$('#new-game-btn').on('click', function () {
+    settings.addClass('hidden');
+    content.removeClass('hidden');
+    $('#new-game-btn').addClass('active');
+    $('#settings-btn').removeClass('active');
+});
