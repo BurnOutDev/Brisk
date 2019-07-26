583826f07de62f3287f8a9e690052c262a78f8c5
Irakli Murusidze <iraklimurusidze@live.com>
2019-07-26 05:26:20 +0400

    Adding User Management Page

--- src/Brisk.ClientApp/src/app/app.module.ts
+++ src/Brisk.ClientApp/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// import { faList, faQuoteRight, faBorderAll, faSave, faTrash, faPlus, faUser, faTimes, faUserLock, faGamepad } from '@fortawesome/free-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { UserManagementComponent } from './user-management/user-management.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    UserManagementComponent
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    // library.add(faList, faGamepad, faQuoteRight, faBorderAll, faSave, faTrash, faPlus, faUser, faTimes, faUserLock);
+    library.add(fas);
+  }
+}
